c8b8c9e3be8aa0f3ac36ab4c72808f53baacf43c
Steven R <stevenr@MacBook-Air-Steven.local>
2024-12-04 22:10:12 +0000

    Añadiendo tests mixtos que hacen uso de ambas clases

--- src/test/java/es/ull/flightspassengers/FlightsPassengersTest.java
+++ src/test/java/es/ull/flightspassengers/FlightsPassengersTest.java
@@ -1,4 +1,47 @@
 package es.ull.flightspassengers;
 
+import es.ull.flights.Flight;
+import es.ull.passengers.Passenger;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FlightsPassengersTest {
+    @DisplayName("Tests de las clases Flight y Passenger")
+    @Nested
+    class TestsBasicosAeropuerto {
+
+        @Test
+        void shouldJoinFlightSuccessfully() {
+            Flight flight = new Flight("AA123", 10);
+            Passenger passenger = new Passenger("12345", "John Doe", "US");
+            passenger.joinFlight(flight);
+            assertEquals(flight, passenger.getFlight());
+            assertEquals(1, flight.getNumberOfPassengers());
+        }
+
+        @Test
+        void shouldSwitchFlightsSuccessfully() {
+            Flight flight1 = new Flight("AA123", 10);
+            Flight flight2 = new Flight("BB234", 10);
+            Passenger passenger = new Passenger("12345", "John Doe", "US");
+
+            passenger.joinFlight(flight1);
+            passenger.joinFlight(flight2);
+
+            assertEquals(flight2, passenger.getFlight());
+            assertEquals(0, flight1.getNumberOfPassengers());
+            assertEquals(1, flight2.getNumberOfPassengers());
+        }
+
+        @Test
+        void shouldThrowExceptionWhenCannotAddToNewFlight() {
+            Flight fullFlight = new Flight("AA123", 0);
+            Passenger passenger = new Passenger("12345", "John Doe", "US");
+
+            assertThrows(RuntimeException.class, () -> passenger.joinFlight(fullFlight));
+        }
+    }
 }
--- src/test/java/es/ull/passengers/PassengersTest.java
+++ src/test/java/es/ull/passengers/PassengersTest.java
@@ -1,6 +1,5 @@
 package es.ull.passengers;
 
-import es.ull.passengers.Passenger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
